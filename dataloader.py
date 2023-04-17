eaa1274299d0bfc221f965d64fb03e5c5c1a26e2
Janan <71359987+Jananzzzz@users.noreply.github.com>
2023-04-18 00:34:23 +0800

    add pdf

---
+++ dataloader.py
@@ -0,0 +1,17 @@
+from datasets import load_dataset
+
+
+
+# bug: Version have no attributes "match"
+
+# load_dataset("visual_genome", "region_descriptions_v1.2.0", data_dir="data")
+# load_dataset("visual_genome", "objects_v1.2.0", data_dir="data")
+# load_dataset("visual_genome", "attributes_v1.2.0", data_dir="data")
+# load_dataset("visual_genome", "relationships_v1.2.0", data_dir="data")
+# load_dataset("visual_genome", "question_answers_v1.2.0", data_dir="data")
+
+
+
+
+
+
