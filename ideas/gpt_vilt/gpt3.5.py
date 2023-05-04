f860f9519369a39b9aa71a3a02833db370682036
Janan <71359987+Jananzzzz@users.noreply.github.com>
2023-05-04 14:22:11 +0800

    move to GPU

--- ideas/gpt_vilt/gpt3.5.py
+++ ideas/gpt_vilt/gpt3.5.py
@@ -1,5 +1,8 @@
 import openai
 from vilt import visual_question_answering
+from pycocotools.coco import COCO
+import numpy as np
+import json
 import time
 
 openai.api_key = "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"
@@ -14,69 +17,99 @@ so i can use the questions to train and build AI models of visual question answe
     },
 ]
 
-caption = "a baseball pitcher winds up to pitch the ball."
-picture_path = "D:/CoT-is-all-you-need/data/coco2017/train2017/000000458752.jpg"
+data_dir = 'D:/CoT-is-all-you-need/data/coco2017'
+data_type = 'train2017'
+annotation_file = f'{data_dir}/annotations/captions_{data_type}.json'
 
-start_time = time.time()
+coco = COCO(annotation_file)
 
-question_generate_message.append({"role": "user", "content": f"{caption}"})
+# Get a list of all image IDs in the dataset
+imgIds = coco.getImgIds()
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=question_generate_message,
-    temperature=0.9,
-)
+# Load the image data and captions for each image
+data = []
+for imgId in imgIds:
+    imgData = coco.loadImgs(imgId)[0]
+    captions = coco.imgToAnns[imgId]
+    for caption in captions:
+        pair = {
+            'image_path': f"{data_dir}/{data_type}/{imgData['file_name']}",
+            'caption': caption['caption']
+        }
+        data.append(pair)
 
-question_response = response.choices[0]['message']['content']
+for i in data:
 
-question_list = []
-for question in question_response.splitlines():
-    print(question[3:])
-    question_list.append(question[3:])
-print("")
+    caption = i['caption']
+    picture_path = i['image_path']
 
-answer_list = []
-for idx,question in enumerate(question_list):
-    answer_list.append(visual_question_answering(picture_path, question))
-    print(idx, question, answer_list[idx])
-print("")
+    # caption = "a baseball pitcher winds up to pitch the ball."
+    # picture_path = "D:/CoT-is-all-you-need/data/coco2017/train2017/000000458752.jpg"
 
-organize_message = [
-    {
-    "role": "user",
-    "content":
-    f"""
-imagine you are a person with strong reasoning ability. 
-Given question-answering pairs, use your reasoning ability to extract useful 
-infomartion to get the final result.
-Both question-answering pairs and final results are provided below:
-```
-question: "{question_list[0]}" Answer: "{answer_list[0]}."
-question: "{question_list[1]}" Answer: "{answer_list[1]}."
-question: "{question_list[2]}" Answer: "{answer_list[2]}."
-question: "{question_list[3]}" Answer: "{answer_list[3]}."
-question: "{question_list[4]}" Answer: "{answer_list[4]}."
-question: "{question_list[5]}" Answer: "{answer_list[5]}."
-question: "{question_list[6]}" Answer: "{answer_list[6]}."
-question: "{question_list[7]}" Answer: "{answer_list[7]}."
-question: "{question_list[8]}" Answer: "{answer_list[8]}."
-question: "{question_list[9]}" Answer: "{answer_list[9]}."
-Final result: {caption}
-```
-Your output  should be a list of useful infomation related to the final result.
-    """
-    }
-]
+    print(caption)
+    print(picture_path)
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=organize_message,
-    temperature=0.9,
-)
+    start_time = time.time()
 
-print(response.choices[0]['message']['content'])
-print("")
+    question_generate_message.append({"role": "user", "content": f"{caption}"})
 
-end_time = time.time()
-print(f"the whole process consumed: {end_time - start_time} seconds")
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=question_generate_message,
+        temperature=0.9,
+    )
+
+    question_response = response.choices[0]['message']['content']
+
+    question_list = []
+    for question in question_response.splitlines():
+        print(question[3:])
+        question_list.append(question[3:])
+    print("")
+
+    answer_list = []
+    for idx,question in enumerate(question_list):
+        answer_list.append(visual_question_answering(picture_path, question))
+        print(idx, question, answer_list[idx])
+    print("")
+
+    organize_message = [
+        {
+        "role": "user",
+        "content":
+        f"""
+    imagine you are a person with strong reasoning ability. 
+    Given question-answering pairs, use your reasoning ability to extract useful 
+    infomartion to get the final result.
+    Both question-answering pairs and final results are provided below:
+    ```
+    question: "{question_list[0]}" Answer: "{answer_list[0]}."
+    question: "{question_list[1]}" Answer: "{answer_list[1]}."
+    question: "{question_list[2]}" Answer: "{answer_list[2]}."
+    question: "{question_list[3]}" Answer: "{answer_list[3]}."
+    question: "{question_list[4]}" Answer: "{answer_list[4]}."
+    question: "{question_list[5]}" Answer: "{answer_list[5]}."
+    question: "{question_list[6]}" Answer: "{answer_list[6]}."
+    question: "{question_list[7]}" Answer: "{answer_list[7]}."
+    question: "{question_list[8]}" Answer: "{answer_list[8]}."
+    question: "{question_list[9]}" Answer: "{answer_list[9]}."
+    Final result: {caption}
+    ```
+    Your output  should be a list of useful infomation related to the final result.
+        """
+        }
+    ]
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=organize_message,
+        temperature=0.9,
+    )
+
+    print(response.choices[0]['message']['content'])
+    print("")
+
+    end_time = time.time()
+    print(f"this image-caption chain of thought reasoing costed : {end_time - start_time} seconds")
+    print("")
 
--- ideas/gpt_vilt/new_vilt.py
+++ ideas/gpt_vilt/new_vilt.py
@@ -1,28 +1,19 @@
+import torch
 from transformers import ViltProcessor, ViltForQuestionAnswering
 from PIL import Image
-import torch
 
 def visual_question_answering(image_path, question):
-
     # prepare image + question
     # image = Image.open("D:/CoT-is-all-you-need/data/coco2017/train2017/000000458752.jpg")
     image = Image.open(image_path)
-
     question = question
 
     processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
     model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 
-    # # prepare inputs
-    # encoding = processor(image, question, return_tensors="pt")
-
-    # # forward pass
-    # outputs = model(**encoding)
-    # logits = outputs.logits
-    # idx = logits.argmax(-1).item()
-    # return model.config.id2label[idx]
-
+    # check if GPU is available
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device:", device)
 
     # move model and inputs to GPU
     model.to(device)
@@ -35,4 +26,4 @@ def visual_question_answering(image_path, question):
         idx = logits.argmax(-1).item()
         return model.config.id2label[idx]
 
-# print(visual_question_answering("D:/CoT-is-all-you-need/data/coco2017/train2017/000000458752.jpg", "what is the color of the ball?"))
+visual_question_answering("D:/CoT-is-all-you-need/data/coco2017/train2017/000000458752.jpg", "what is the color of the ball?")
