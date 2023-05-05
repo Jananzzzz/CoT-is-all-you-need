a7aa2d5ded7d0d41922b36ad97b40052def0a85d
Janan <71359987+Jananzzzz@users.noreply.github.com>
2023-05-05 20:14:24 +0800

    generate chain of thought in batches.

--- ideas/gpt_vilt/gpt3.5.py
+++ ideas/gpt_vilt/gpt3.5.py
@@ -1,9 +1,9 @@
 import openai
 from vilt import visual_question_answering
 from pycocotools.coco import COCO
-import numpy as np
 import json
 import time
+import random
 
 openai.api_key = "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"
 
@@ -33,119 +33,142 @@ for imgId in imgIds:
     captions = coco.imgToAnns[imgId]
     for caption in captions:
         pair = {
+            'image_id': imgId,
             'image_path': f"{data_dir}/{data_type}/{imgData['file_name']}",
             'caption': caption['caption']
         }
         data.append(pair)
 
-for i in data:
 
-    caption = i['caption']
-    picture_path = i['image_path']
+for i in range(500):
 
+    item = data[random.randint(0, len(data))]
+
+    image_id = item['image_id']
+    caption = item['caption']
+    picture_path = item['image_path']
+
+    # image_id = 458752
     # caption = "a baseball pitcher winds up to pitch the ball."
     # picture_path = "D:/CoT-is-all-you-need/data/coco2017/train2017/000000458752.jpg"
 
     print(caption)
     print(picture_path)
 
-    start_time = time.time()
+    try:
 
-    question_generate_message.append({"role": "user", "content": f"{caption}"})
+        start_time = time.time()
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=question_generate_message,
-        temperature=0.9,
-    )
+        question_generate_message.append({"role": "user", "content": f"{caption}"})
 
-    question_response = response.choices[0]['message']['content']
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=question_generate_message,
+            temperature=0.9,
+        )
 
-    question_list = []
-    for question in question_response.splitlines():
-        print(question[3:])
-        question_list.append(question[3:])
-    print("")
+        question_response = response.choices[0]['message']['content']
 
-    answer_list = []
-    for idx,question in enumerate(question_list):
-        answer_list.append(visual_question_answering(picture_path, question))
-        print(idx, question, answer_list[idx])
-    print("")
+        question_list = []
+        for question in question_response.splitlines():
+            print(question[3:])
+            question_list.append(question[3:])
+        print("")
+
+        answer_list = []
+        for idx,question in enumerate(question_list):
+            answer_list.append(visual_question_answering(picture_path, question))
+            print(idx, question, answer_list[idx])
+        print("")
 
 
-    organize_message = [{
-        "role": "user",
-        "content": f"""imagine you are a person with strong reasoning ability. 
+        organize_message = [{
+            "role": "user",
+            "content": f"""imagine you are a person with strong reasoning ability. 
 Given question-answering pairs, use your reasoning ability to extract useful \
 infomartion to get the final result. 
 Both question-answering pairs and final results are provided below:
-'''
-"""
-    }
-    ]
-
-    # add 5 questions and answers to test_message
-    for i in range(len(question_list)):
-        organize_message[0]["content"] += f"""question: '{question_list[i]}' Answer: '{answer_list[i]}.'
-"""
-
-    organize_message[0]["content"] += f"""Final result: {caption}
-'''
-Your output  should be a list of useful infomation related to the final result."""
-
-    print("")
-    print(organize_message[0]["content"])
-    print("")
-
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=organize_message,
-        temperature=0.9,
-    )
-
-    print(response.choices[0]['message']['content'])
-    print("")
-
-    refine_message0 = [
-        {
-            "role": "user",
-            "content": f"""Here is a caption: "{caption}".
-Below are some information that may help deduce the caption.
-Please identify which information is useful, ouput in a list without any other prompt or prefix.
-"""
+'''      
+"""      
         }
-    ]
+        ]
 
-    refine_message = [
-        {
-            "role": "user",
-            "content": f"""Here is a caption: "{caption}".
+        # add 5 questions and answers to test_message
+        for i in range(len(question_list)):
+            organize_message[0]["content"] += f"""question: '{question_list[i]}' Answer: '{answer_list[i]}.'
+"""      
+
+        organize_message[0]["content"] += f"""Final result: {caption}
+'''      
+Your output should be a list of statement sentences of useful infomation related to the final result, without any prefix or prompt."""
+
+        print("")
+        print(organize_message[0]["content"])
+        print("")
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=organize_message,
+            temperature=0.9,
+        )
+
+        print(response.choices[0]['message']['content'])
+        print("")
+
+        refine_message = [
+            {
+                "role": "user",
+                "content": f"""Here is a caption: "{caption}".
 Below are some information that related to the caption.
-Please identify which information can deduce the caption, ouput in a list without any other prompt or prefix.
-"""
-        }
-    ]
+Please identify which information can deduce the caption, output in a list without any other prompt or prefix.
+"""      
+            }
+        ]
 
-    for info in response.choices[0]['message']['content'].splitlines():
-        refine_message[0]["content"] += f"""{info}
-"""
+        for info in response.choices[0]['message']['content'].splitlines():
+            refine_message[0]["content"] += f"""{info}
+"""      
 
-    print(refine_message[0]["content"])
+        print(refine_message[0]["content"])
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=refine_message,
-        temperature=0.9,
-    )
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=refine_message,
+            temperature=0.9,
+        )
 
-    print(response.choices[0]['message']['content'])
+        print(response.choices[0]['message']['content'])
+
+        results = []
+        for result in response.choices[0]['message']['content'].splitlines():
+            results.append(result[2:])
+        
+        with open('D:/CoT-is-all-you-need/ideas\gpt_vilt/chain-of-thought.json', 'r+') as f:
+            thoughts = json.load(f)
+            new_thought = {
+                "image_id": image_id,
+                "caption": caption,
+                "chain-of-thought": results
+            }
+            thoughts.append(new_thought)
+            f.seek(0)
+            json.dump(thoughts, f, indent=4)
+            f.truncate()
 
 
-    end_time = time.time()
-    print(f"this image-caption chain of thought reasoing costed : {end_time - start_time} seconds")
-    print("")
+        end_time = time.time()
+        print(f"this image-caption chain of thought reasoning costed : {end_time - start_time} seconds")
+        print("")
 
-    print("first test done")
-    break
+        # if (end_time - start_time) < 60:
+        #     time.sleep(61 - (end_time - start_time))
+        time.sleep(60)
+
+
+        print(f"Chain of thought reasoning for {caption} finished.")
+
+    except Exception as e:
+        print(e)
+        print(f"Chain of thought reasoning for {caption} failed.")
+        time.sleep(60)
 
